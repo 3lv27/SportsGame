60836a35f88f803e06306f698feb08a47083fe48
ventdlnord <ventdlnord@gmail.com>
2017-11-22 03:40:26 +0100

    buttons owner and search continue...

--- models/challenge.js
+++ models/challenge.js
@@ -5,28 +5,28 @@ const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
 const challengerSchema = new Schema({
-  challengerName: String,
-  owner: {
-    type: ObjectId,
-    ref: 'User'
-  },
-  location: {
-    latitud: Number,
-    longitud: Number
-  },
-  sports: {
-    type: [String],
-    enum: ['SkateBoarding', 'BMX', 'Parkour', 'Fitness', 'RollerSkating']
-  },
-  description: String,
-  linkValidation: String,
-  timelimit: Date,
-  enrolled: {
-    type: ObjectId,
-    ref: 'User'
-  }
+    challengerName: String,
+    owner: {
+        type: ObjectId,
+        ref: 'User'
+    },
+    location: {
+        latitud: Number,
+        longitud: Number
+    },
+    sports: {
+        type: [String],
+        enum: ['SkateBoarding', 'BMX', 'Parkour', 'Fitness', 'RollerSkating']
+    },
+    description: String,
+    linkValidation: String,
+    timeLimit: Date,
+    enrolled: {
+        type: [ObjectId],
+        ref: 'User'
+    }
 }, {
-  timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 });
 
 const Challenger = mongoose.model('Challenger', challengerSchema);
