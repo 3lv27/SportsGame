2ff4ba1dc73a7f5b9a1537bdfb5f101ddb706da0
bryan <supercrew3@gmail.com>
2017-11-23 18:00:17 +0100

    little change

--- models/challenge.js
+++ models/challenge.js
@@ -5,30 +5,30 @@ const Schema = mongoose.Schema;
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const challengeSchema = new Schema({
-    challengeName: String,
-    owner: {
-        type: ObjectId,
-        ref: 'User'
-    },
-    location: {
-        latitud: Number,
-        longitud: Number
-    },
-    sports: {
-        type: String,
-        enum: ['SkateBoarding', 'BMX', 'Parkour', 'Fitness', 'RollerSkating']
-    },
-    description: String,
-    linkValidation: String,
-    timelimit: Date,
-    enrolled: {
-        type: ObjectId,
-        ref: 'User'
-    }
+  challengeName: String,
+  owner: {
+    type: ObjectId,
+    ref: 'User'
+  },
+  location: {
+    latitud: Number,
+    longitud: Number
+  },
+  sports: {
+    type: String,
+    enum: ['SkateBoarding', 'BMX', 'Parkour', 'Fitness', 'RollerSkating']
+  },
+  description: String,
+  linkValidation: String,
+  timelimit: Date,
+  enrolled: {
+    type: ObjectId,
+    ref: 'User'
+  }
 }, {
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
-});
+  });
 
-const Challenge = mongoose.model('Challenge', challengerSchema);
+const Challenge = mongoose.model('Challenge', challengeSchema);
 
 module.exports = Challenge;
